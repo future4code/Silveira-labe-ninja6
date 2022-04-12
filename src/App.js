1d81f4b8bd5930e2d3b1c8dee8b246d0e12a0f52
Davi Souza <davi.231souza@gmail.com>
2022-04-12 17:13:24 -0300

    Componente de registro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import React from 'react'
-import { AppContainer } from './components/AppContainer'
+import React from 'react';
+import { AppContainer } from './components/AppContainer';
+import CreateJob from './components/CreateJob/CreateJob';
 
-function App() {
-	return (
-        <AppContainer />
-	)
-}
+class App extends React.Component {
+	state = {
+		currentScreen: ""
+	};
+
+	goToCreateJob = () => this.setState({currentScreen: "register"});
+	goToCards = () => this.setState({currentScreen: "cards"});
+	goToHome = () => this.setState({currentScreen: "home"});
+
+	changeScreen = () => {
+		switch (this.state.currentScreen) {
+			case "register":
+			
+				return (
+					<CreateJob goToHome={this.goToHome}/>
+				)
+			
+			case "cards":
+			
+				return (
+					<div>
+						<h1>cards</h1>
+						<button onClick={this.goToHome}>Home</button>
+					</div>
+				)
+
+			case "home":
+
+				return (
+					<AppContainer 
+						goToCreateJob={this.goToCreateJob} 
+						goToCards={this.goToCards}
+					/>
+				)
+
+			default:
+
+				return (
+					<AppContainer 
+						goToCreateJob={this.goToCreateJob} 
+						goToCards={this.goToCards}
+					/>
+				)
+		};
+	};
+	render () {
+		return	(
+			<div>
+				{this.changeScreen()}
+			</div>
+		)
+	};
+};
 
 export default App
