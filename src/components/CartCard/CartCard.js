0cd50ba63662093cc60821ef05782276a55ece90
Davi Souza <davi.231souza@gmail.com>
2022-04-17 19:00:06 -0300

    conserto carrinho

--- src/components/CartCard/CartCard.js
+++ src/components/CartCard/CartCard.js
@@ -7,7 +7,7 @@ const CartCard = (props) => {
         <CardContainer>
             <h3>{props.title}</h3>
             <p>R${props.price.toFixed(2)}</p>
-            <button className="button-detail" onClick={() => props.removeCart(props.id)}>Remover</button>
+            <button className="button-detail" onClick={() => props.removerCart(props.id)}>Remover</button>
         </CardContainer>
     )
 }
